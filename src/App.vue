fe0066d2ef7e777d2fa6f29b81ec2139a546215f
peer <peer@infi.nl>
2019-05-26 16:41:48 +0200

    proxy image & more slides

--- src/App.vue
+++ src/App.vue
@@ -16,19 +16,19 @@
           .row
             .col-50
               ul
-                li.fragment Virtual Dom
-                li.fragment Great CLI/UI
-                li.fragment User-backed
-                li.fragment PWA / SSR
-                li.fragment Easy code-splitting
+                li.fragment(data-fragment-index="1") Virtual Dom
+                li.fragment(data-fragment-index="3") Great CLI/UI
+                li.fragment(data-fragment-index="5") User-backed
+                li.fragment(data-fragment-index="7") PWA / SSR
+                li.fragment(data-fragment-index="9") Easy code-splitting
             .col-50
               ul
-                li.fragment Avoiding bias
-                li.fragment 
+                li.fragment(data-fragment-index="2") Avoiding bias
+                li.fragment(data-fragment-index="4") 
                   a(href="https://stefankrause.net/js-frameworks-benchmark8/table.html") Exceptionally fast
-                li.fragment Modular libs
-                li.fragment Great ecosystem
-                li.fragment Reactive
+                li.fragment(data-fragment-index="6") Modular libs
+                li.fragment(data-fragment-index="8") Great ecosystem
+                li.fragment(data-fragment-index="10") Reactive
 
         section
           h2 What is reactivity?
@@ -36,6 +36,23 @@
           p.fragment(data-fragment-index="2") ...anything depending on that state should update as well
           h2.fragment(data-fragment-index="3") Updating stuff updates other stuff
 
+        section
+          pre(style="width: 50%;")
+            code.hljs(data-trim data-noescape style="padding: 1em;")
+              | new Vue({
+              |   data: {
+              |     price: 22.99,
+              |     amount: 3
+              |   },
+              |   computed: {
+              |     totalPrice() {
+              |       return this.price*this.amount
+              |     }
+              |   },
+              |   render: function(h) { 
+              |     return h('p', `€ ${this.totalPrice}`)
+              |   },
+              | })
 
         section
           h2 Reactivity demo
@@ -67,11 +84,59 @@
             li.fragment Dynamically adding properties
           p.fragment(style="font-size: 92px") {{ '=> Use Vue.set()' }}
 
+        section(style="content-align: center;")
+          h2 Use Vue.set()
+          div(style="width: 80%; margin: auto; font-size: 64px;")
+            .col-50(style="padding-left: 0.1em; padding-right: 0.1em")
+              .fragment(data-fragment-index="1")
+                pre(style="padding: 0.2em; width: 100%; margin: 0; margin-bottom: 1em")
+                  code.hljs(data-trim data-noescape) myArray[8] = 'hi'
+              .fragment(data-fragment-index="3")
+                pre(style="padding: 0.2em; width: 100%; margin: 0;")
+                  code.hljs(data-trim data-noescape) myObject.newProp = 'hi'
+            .col-50(style="padding-left: 0.1em; padding-right: 0.1em;")
+              .fragment(data-fragment-index="2") 
+                pre(style="padding: 0.2em; width: 100%; margin: 0; margin-bottom: 1em")
+                  code.hljs(data-trim data-noescape) Vue.set(myArray, 8, 'hi')
+              .fragment(data-fragment-index="4")
+                pre(style="padding: 0.2em; width: 100%; margin: 0;")
+                  code.hljs(data-trim data-noescape) Vue.set(myObject, 'newProp', 'hi')
+
         section
-          p Proxies: only Evergreen browsers/no polyfills
+          h2 Proxies
+          .col-50
+            pre.fragment
+              code.hljs(data-trim data-noescape) new Proxy(target, handler)
+            pre.fragment
+              code.hljs(data-trim data-noescape)
+                | function observe(data) {
+                |   const observedData = new Proxy(data, {
+                |     get() {
+                |
+                |     },
+                |     set() {
+                |
+                |     },
+                |     deleteProperty() {
+                |
+                |     },
+                |   })
+                | }
+          .col-50
+            img(src="@pics/proxy.png" height="400")
+        
+        section
+          h2 Proxies
+          p Only supported in Evergreen browsers
+          p.fragment Use <span style="color: orange">2.x-next</span> if you don't need to support <span style="color: cornflowerblue">IE11</span>
+          p.fragment Use <span style="color: orange">2.x</span> if you do 
+            span.fragment (don't fortget <span style="color: lime">Vue.set()</span>)
 
         section
           h2 unit test demo
+        
+        section
+          h2 events
         
         section
           h2 Thank you!
@@ -111,6 +176,7 @@ export default {
 <style lang="scss">
 @import '~reveal.js/css/reveal.css';
 @import '~reveal.js/css/theme/black.css';
+@import '~reveal.js/lib/css/monokai.css';
 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
